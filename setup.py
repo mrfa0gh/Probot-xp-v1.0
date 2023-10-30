9fdce282a0073c1f400dacbcf0de7a8babc0390f
Fares ghalwash <117339225+mrfa0gh@users.noreply.github.com>
2023-10-30 22:29:44 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from os import system
+
+system('pip install requests')
+system('pip install random')
+system('pip install time')
+print('\nFinished Best Wishes:By : Ghalwash .')
+system('pause >NUL')
