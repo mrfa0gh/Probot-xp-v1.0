8f1d4f14e323660c94664bc6e64a117565b496ec
Fares ghalwash <117339225+mrfa0gh@users.noreply.github.com>
2023-10-30 22:32:21 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from os import system
 system('pip install requests')
 system('pip install random')
 system('pip install time')
+system('pip install pyfiglet')
 print('\nFinished Best Wishes:By : Ghalwash .')
 system('pause >NUL')
